ebbddb0dc8c756f80ba88c6c164fff0d848b3186
lfmontesanto <leandro.montesanto@wolox.com.ar>
2020-02-05 11:36:46 -0300

    eslint style corrections

--- finger-exercises/ecmascript/01 - spread/index.js
+++ finger-exercises/ecmascript/01 - spread/index.js
@@ -2,43 +2,39 @@ import { isArray } from './utils';
 
 export function min(argument) {
   if (argument == null) {
-    return undefined
-  } else {
-    if (isArray(argument)) {
-      let minimums = argument.map(element => isArray(element) ? Math.min(...element) : element)
-      return Math.min(...minimums)
-    } else {
-      return Math.min(...arguments)
-    }
+    return undefined;
   }
+  if (isArray(argument)) {
+    const minimums = argument.map(element => (isArray(element) ? Math.min(...element) : element));
+    return Math.min(...minimums);
+  }
+  return Math.min(...arguments);
 }
 
 export function copy(argument) {
   if (argument == null) {
-    return undefined
-  } else {
-    if (isArray(argument)){
-      return argument.map(element => ({...element}))
-    }
-    return {...argument }
+    return undefined;
   }
+  if (isArray(argument)) {
+    return argument.map(element => ({ ...element }));
+  }
+  return { ...argument };
 }
 
 export function reverseMerge(argument) {
-  let reversedArray = []
+  const reversedArray = [];
   if (argument == null) {
-    return undefined
-  } else {
-    Array.from(arguments).slice(0).reverse().map(element => reversedArray.push(...element))
+    return undefined;
   }
-  return reversedArray
+  Array.from(arguments).slice(0).reverse().map(element => reversedArray.push(...element));
+
+  return reversedArray;
 }
 
-export function filterAttribs (argument) {
+export function filterAttribs(argument) {
   if (argument == null) {
-    return undefined
-  } else {
-    let {a,b, ...filtered} = argument
-    return filtered
+    return undefined;
   }
+  const { a, b, ...filtered } = argument;
+  return filtered;
 }
