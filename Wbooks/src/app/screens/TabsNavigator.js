671b786c39616c7083fd3e4692a1d2d49753278a
lfmontesanto <leandro.montesanto@wolox.com.ar>
2020-02-17 14:30:16 -0300

    dummy screen

--- Wbooks/src/app/screens/TabsNavigator.js
+++ Wbooks/src/app/screens/TabsNavigator.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import StackNavigator from './StackNavigator';
 
-const Tab = createBottomTabNavigator  ();
+import StackNavigator from './StackNavigator';
+import dummyScreenContainer from './dummyScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
-  return(
-    <TabNavigator>
-        <Tab.Screen name = "Home" component={StackNavigator}/> 
-    </TabNavigator>
-  )
-
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Library" component={StackNavigator} />
+      <Tab.Screen name="WishList" component={dummyScreenContainer} />
+    </Tab.Navigator>
+  );
 }
---
+++ Wbooks/src/app/screens/dummyScreen/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function dummyScreenContainer() {
+  return (
+    <View>
+      <Text>Screen</Text>
+    </View>
+  );
+}
+
+export default dummyScreenContainer;
